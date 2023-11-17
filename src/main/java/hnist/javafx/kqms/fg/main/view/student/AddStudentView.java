912320ba0dd00b4ab8e0a970cc0cef8c8f830935
DisappearingSnow <1960313113@qq.com>
2023-11-17 16:36:07 +0800

    update

--- src/main/java/hnist/javafx/kqms/fg/main/view/student/AddStudentView.java
+++ src/main/java/hnist/javafx/kqms/fg/main/view/student/AddStudentView.java
@@ -3,6 +3,7 @@ package hnist.javafx.kqms.fg.main.view.student;
 import hnist.javafx.kqms.bg.controller.StudentController;
 import hnist.javafx.kqms.fg.main.view.View;
 import hnist.javafx.kqms.pojo.Student;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -20,35 +21,38 @@ public class AddStudentView extends View {
     @Override
     public Pane initView() {
         Label promptLabel =new Label("请填写必要信息");
+        Label studentNumberLabel = new Label("学号");
         TextField studentInfoNumber = new TextField();
-        studentInfoNumber.setPromptText("学号");
+        Label studentNameLabel = new Label("姓名");
         TextField studentInfoName = new TextField();
-        studentInfoName.setPromptText("姓名");
+        Label stduentSexLabel = new Label("性别");
         TextField studentInfoSex = new TextField();
-        studentInfoSex.setPromptText("性别");
+        Label studentAgeLabel = new Label("年龄");
         TextField studentInfoAge = new TextField();
-        studentInfoAge.setPromptText("年龄");
+        Label studentClassLabel = new Label("班级");
         TextField studentInfoClass = new TextField();
-        studentInfoClass.setPromptText("班级");
         Button addStudentInfoButton = new Button("添加");
+        addStudentInfoButton.setPrefSize(200,10);
 
-        promptLabel.setLayoutX(100);
-        promptLabel.setLayoutY(100);
-        studentInfoNumber.setLayoutX(100);
-        studentInfoNumber.setLayoutY(300);
-        studentInfoName.setLayoutX(250);
-        studentInfoName.setLayoutY(300);
-        studentInfoSex.setLayoutX(400);
-        studentInfoSex.setLayoutY(300);
-        studentInfoAge.setLayoutX(550);
-        studentInfoAge.setLayoutY(300);
-        studentInfoClass.setLayoutX(700);
-        studentInfoClass.setLayoutY(300);
-        addStudentInfoButton.setLayoutX(850);
-        addStudentInfoButton.setLayoutY(300);
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.add(promptLabel,1,0);
+        gridPane.add(studentNumberLabel,1,1);
+        gridPane.add(studentInfoNumber,2,1);
+        gridPane.add(studentNameLabel,1,2);
+        gridPane.add(studentInfoName,2,2);
+        gridPane.add(stduentSexLabel,1,3);
+        gridPane.add(studentInfoSex,2,3);
+        gridPane.add(studentAgeLabel,1,4);
+        gridPane.add(studentInfoAge,2,4);
+        gridPane.add(studentClassLabel,1,5);
+        gridPane.add(studentInfoClass,2,5);
+        gridPane.add(addStudentInfoButton,1,6);
+        GridPane.setConstraints(addStudentInfoButton,1,6,2,1);
 
-        Pane pane = new Pane();
-        pane.getChildren().addAll(promptLabel, studentInfoNumber, studentInfoName, studentInfoSex, studentInfoAge, studentInfoClass, addStudentInfoButton);
+        gridPane.setAlignment(Pos.CENTER);
+
 
         //提取输入框中的学生信息
         addStudentInfoButton.setOnAction(e -> {
@@ -60,11 +64,8 @@ public class AddStudentView extends View {
             } else {
                 //学号重复
                 Label repeatLabel = new Label("学号重复！");
-                repeatLabel.setLayoutX(100);
-                repeatLabel.setLayoutY(350);
                 repeatLabel.setTextFill(Color.RED);
-
-                pane.getChildren().add(repeatLabel);
+                gridPane.add(repeatLabel,1,7);
             }
 
             studentInfoNumber.clear();
@@ -75,7 +76,8 @@ public class AddStudentView extends View {
         });
 
 
+
         // TODO
-        return pane;
+        return gridPane;
     }
 }
