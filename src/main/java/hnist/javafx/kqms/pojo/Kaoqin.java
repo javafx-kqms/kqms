142b875c96d9623acaf0c5a3dfe38a6c989e37d4
DisappearingSnow <1960313113@qq.com>
2023-11-28 22:18:26 +0800

    update

--- src/main/java/hnist/javafx/kqms/pojo/Kaoqin.java
+++ src/main/java/hnist/javafx/kqms/pojo/Kaoqin.java
@@ -36,7 +36,7 @@ public class Kaoqin {
                 "   缺课日期：" + date +
                 "   课程名称：" + courseName +
                 "   缺课节次：" + section +
-                "   缺课类型" + type;
+                "   缺课类型：" + type;
     }
 
     public Date getDate() {
